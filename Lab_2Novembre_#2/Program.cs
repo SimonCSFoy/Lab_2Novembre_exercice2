5f64e8bbbb502aa8e82c2e7c90d36d5756b6fea1
sbelliveau <sbelliveau@ORD-98475>
2023-11-02 12:51:21 -0400

    Ajoutez des fichiers projet.

---
+++ Lab_2Novembre_#2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Lab_2Novembre__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int nombre = ObtenirEntierUtilisateur("Entrez un nombre entier (10 caractères maximum) : ");
+                if (EstPair(nombre))
+                {
+                    Console.WriteLine($"Le nombre {nombre} est pair");
+                }
+                else
+                {
+                    Console.WriteLine($"Le nombre {nombre} n'est pas pair");
+                }
+            }
+        }
+
+        public static int ObtenirEntierUtilisateur(string message)
+        {
+            bool conversionReussie = false;
+            int nombre = 0;
+            while(conversionReussie != true)
+            {
+                string entreeUtilisateur = ObtenirStringUtilisateur(message);
+                conversionReussie = int.TryParse(entreeUtilisateur, out nombre);
+            }
+            return nombre;
+        }
+
+        public static string ObtenirStringUtilisateur(string message)
+        {
+            Console.WriteLine("");
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+        public static bool EstPair(int nombre)
+        {
+            return nombre % 2 == 0;
+        }
+    }
+}
